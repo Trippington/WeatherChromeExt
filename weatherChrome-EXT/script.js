34bfd7b893f619d05010662df0f16c9dff515aad
Clarence Durham <tripp.durham@yahoo.com>
2023-03-07 11:34:05 -0600

    3-7 updates

--- weatherChrome-EXT/script.js
+++ weatherChrome-EXT/script.js
@@ -18,6 +18,10 @@ async function fetchData() {
         humidity = tdata.current.humidity;
         uv = tdata.current.uv;
         wind_mph = tdata.current.wind_mph;
+        wind_dir = tdata.current.wind_dir;
+        feelslike_f = tdata.current.feelslike_f;
+        gust_mph = tdata.current.gust_mph;
+
 
         const maxtemp_f_day0 = tdata.forecast.forecastday[0].day.maxtemp_f;
         const maxtemp_f_day1 = tdata.forecast.forecastday[1].day.maxtemp_f;
@@ -26,6 +30,19 @@ async function fetchData() {
         const mintemp_f_day0 = tdata.forecast.forecastday[0].day.mintemp_f;
         const mintemp_f_day1 = tdata.forecast.forecastday[1].day.mintemp_f;
         const mintemp_f_day2 = tdata.forecast.forecastday[2].day.mintemp_f;
+        
+
+        // hard coded test data
+        const weatherData = {"condition":{"text":"Overcast","icon":"https://cdn.weatherapi.com/weather/64x64/day/122.png","code":1009}};
+    
+        // Extract text and icon from the API response
+        const weatherText = weatherData.condition.text;
+        const weatherIcon = weatherData.condition.icon;
+
+        // Update HTML elements with weather data
+        document.getElementById("weather-text").textContent = weatherText;
+        document.getElementById("weather-icon").setAttribute("src", weatherIcon);
+        //end of test section
 
 
         //myJavaScriptVar = tdata.current.toString();
@@ -54,7 +71,12 @@ async function fetchData() {
         document.getElementById("3l").innerHTML = mintemp_f_day2 + "&#176";;
 
 
-        document.getElementById("wind").innerHTML = wind_mph + "mph";;
+        document.getElementById("gust").innerHTML = gust_mph + "mph";
+        
+        document.getElementById("feelsLike").innerHTML = feelslike_f + "&#176";;
+
+
+        document.getElementById("wind").innerHTML = wind_dir + " " + wind_mph + "mph";;
 
         // current UV index
         document.getElementById("uv_index").innerHTML = uv;;
@@ -72,3 +94,22 @@ async function fetchData() {
     }
 }
 fetchData();
+
+
+function updateClock() {
+	// Get the current time and date
+	let now = new Date();
+
+	// Format the time and date strings
+    const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	let time = now.toLocaleTimeString();
+	let date = now.toLocaleDateString();
+
+	// Update the HTML elements
+	document.getElementById('clock').innerHTML = time;
+	document.getElementById('date').innerHTML = date;
+    document.getElementById("timezone").textContent = userTimezone;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
